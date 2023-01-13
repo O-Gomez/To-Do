6f6016e66072e10d3aba735bbdc3d4d7bce29d6a
O-Gomez <mattpollock0@unitec.edu>
2023-01-12 23:24:48 -0600

    Mejora y limpieza de codigo

--- ToDo/JS/script.js
+++ ToDo/JS/script.js
@@ -1,53 +1,41 @@
+let formuInput = document.getElementById('agg_tarea');//Parte para mantener el foco en input
+
 jQuery(document).ready(function () {
-    /* AGREGAR TAREAS */
+    //AGREGAR TAREAS
     $(document).on('submit', '#agregar_form', function (e) {
         e.preventDefault();
-        var inf = $('#agregar_form').serialize();
+        let inf = $('#agregar_form').serialize();
         //console.log(inf);
 
         $.ajax({
             url: '../PHP/tar_agg.php',
             type: 'POST',
             data: inf,
-
-            success: function (fun) {
-                $('#lista').load('../APP/index.php #lista'),
-                    $('#aaaa').load('../APP/index.php #aaaa');
+            success: function () {
+                $('#lista').load('../APP/index.php #lista');
+                $('.toDo-scrollbar').mCustomScrollbar("scrollTo", "bottom", {
+                    scrollInertia: 500
+                });
+                setTimeout(function () {
+                    $('.toDo-scrollbar').mCustomScrollbar("scrollTo", "-=500");
+                }, 550);
             }
         });
+        document.getElementById('agregar_form').reset();//Para focus y limpieza
     });
 
-    /* POR SI NO SE USA EL BOTON DE ACTUALIZAR */
+    //POR SI NO SE USA EL BOTON DE ACTUALIZAR
     $(document).on('submit', 'div.act form#act_form', function (e) {
         e.preventDefault();
-        /* ACTIVA EL BOTON PARA ENVIAR LA INFORMACION POR LA FUNCION 'actualizar' */
+        //ACTIVA EL BOTON PARA ENVIAR LA INFORMACION POR LA FUNCION 'actualizar'
         $('div.act #act_form button').click();
     });
 });
 
-/* MARCAR Y DESMARCAR TAREAS */
-function marca(val1, val2) {
-    if (val2 == "1") {
-        val2 = "0";
-    } else if (val2 == "0") {
-        val2 = "1";
-    }
-    //console.log(val1 + ' ' + val2);
-    $.ajax({
-        url: '../PHP/tar_marca.php',
-        type: 'POST',
-        data: { v1: val1, v2: val2 },
-
-        success: function (fun) {
-            $('.a' + val1).load('../APP/index.php .a' + val1);
-        }
-    });
-};
-
-/* HABILITAR EDICION DE TAREA */
+//HABILITAR EDICION DE TAREA
 function editable(val1) {
-    var aa = document.getElementById('a' + val1);
-    var bb = document.getElementById('b' + val1);
+    let aa = document.getElementById('tareaMarcable' + val1);
+    let bb = document.getElementById('tareaEditable' + val1);
 
     aa.classList.toggle('ocultar');
     bb.classList.toggle('ocultar');
@@ -55,41 +43,56 @@ function editable(val1) {
     document.getElementById('act_tarea' + val1).focus();
 };
 
-/* VACIAR LISTA */
-function vaciar() {
-    $.ajax({
-        url: '../PHP/tar_vaciar.php',
-
-        success: function (fun) {
-            $('#lista').load('../APP/index.php #lista');
-        }
-    });
-};
-
-/* ACTUALIZA LA TAREA */
+//ACTUALIZA LA TAREA
 function actualizar(val1) {
-    var val2 = document.querySelector('#act_tarea' + val1).required;
-    var val2 = document.querySelector('#act_tarea' + val1).value;
-    //console.log(val1 + ' ' + val2);
-
+    let val2 = document.querySelector('#act_tarea' + val1).value;
+    //var val2 = document.querySelector('#act_tarea' + val1).getAttribute('name').substring(9);
     $.ajax({
         url: '../PHP/tar_act.php',
         type: 'POST',
         data: { v1: val1, v2: val2 },
-
-        success: function (fun) {
-            $('#lista').load('../APP/index.php #lista');
+        success: function () {
+            $('#tareaMarcable' + val1).load('../APP/index.php #tareaMarcable' + val1),
+                editable(val1);
+            console.log("lal");
         }
     });
+    formuInput.focus();
 };
 
-/* ELIMINA TAREAS SELECCIONADAS/MARCADAS/COMPLETADAS */
-function seleccionados() {
+//MARCAR Y DESMARCAR TAREAS
+function marca(val1, val2) {
+    if (val2 == "1") val2 = "0";
+    else if (val2 == "0") val2 = "1";
+    $.ajax({
+        url: '../PHP/tar_marca.php',
+        type: 'POST',
+        data: { v1: val1, v2: val2 },
+        success: function () {
+            $('#tareaMarcable' + val1).load('../APP/index.php #tareaMarcable' + val1);
+        }
+    });
+    formuInput.focus();
+};
+
+//VACIAR LISTA
+document.getElementById('btnVaciar').addEventListener('click', function () {
+    $.ajax({
+        url: '../PHP/tar_vaciar.php',
+        success: function () {
+            $('#lista').load('index.php #lista');
+        }
+    });
+    formuInput.focus();
+});
+
+//ELIMINA TAREAS SELECCIONADAS/MARCADAS/COMPLETADAS
+document.getElementById('btnEliminar').addEventListener('click', function () {
     $.ajax({
         url: '../PHP/tar_seleccionados.php',
-
-        success: function (fun) {
-            $('#lista').load('../APP/index.php #lista');
+        success: function () {
+            $('#lista').load('index.php #lista');
         }
-    })
-};
+    });
+    formuInput.focus();
+});
